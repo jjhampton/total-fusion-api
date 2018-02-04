ae19ed6d82709050c3fc300b5442b957ee3c8a00
John Hampton <john_j_hampton@yahoo.com>
2018-02-04 01:45:34 -0500

    Move RavenDB url / database to appsettings.json config values

--- DocumentStoreHolder.cs
+++ DocumentStoreHolder.cs
@@ -1,10 +1,12 @@
+using Microsoft.Extensions.Configuration;
 using System;
 using Raven.Client.Documents;
-
+using System.IO;
 
 namespace TotalFusionApi
 {
     public class DocumentStoreHolder
+
     {
         private static Lazy<IDocumentStore> store = new Lazy<IDocumentStore>(CreateStore);
 
@@ -12,10 +14,18 @@ namespace TotalFusionApi
 
         private static IDocumentStore CreateStore()
         {
+            var Configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            String url = Configuration["Raven:Url"];
+            String database = Configuration["Raven:Database"];
+
             IDocumentStore store = new DocumentStore()
             {
-                Urls = new[] { "http://localhost:8080" },
-                Database = "TotalFusion"
+                Urls = new[] { url },
+                Database = database
             }.Initialize();
 
             return store;
